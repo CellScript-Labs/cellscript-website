8d2adbf9dd02fb52ad38581e9024272cc8fb6454
Arthur Zhang <arthur@avatolabs.com>
2026-08-11 19:39:06 +0800

    release: align website with CellScript 0.23.0

--- public/playground-worker.js
+++ public/playground-worker.js
@@ -1,6 +1,6 @@
 let wasmModulePromise;
 let wasmModule;
-const COMPILER_ASSET_VERSION = "20260731-v0.22.0-9bb2d765";
+const COMPILER_ASSET_VERSION = "20260811-v0.23.0-fa369818";
 const CELLSCRIPT_EDITION = "2026";
 const COMPILER_LOAD_TIMEOUT_MS = 12_000;
 let latestCompileId = 0;
